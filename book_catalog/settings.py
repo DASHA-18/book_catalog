7f9aa16437358a68493e76d8f371dc178643c7c7
Brueva Dasha <darabrueva65@gmail.com>
2026-06-01 10:24:56 +0300

    исправила стили

--- book_catalog/settings.py
+++ book_catalog/settings.py
@@ -72,6 +72,10 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'books' / 'styles',
+    BASE_DIR / 'books' / 'scripts',
+]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
